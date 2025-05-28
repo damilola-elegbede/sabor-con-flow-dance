3eb508533ecd26826247019a51ff9d2f9d0334cb
Damilola Elegbede <damilola.elegbede@gmail.com>
2025-05-28 12:34:16 -0600

    Update Vercel handler to use BaseHTTPRequestHandler

--- api/index.py
+++ api/index.py
@@ -1,4 +1,18 @@
+from http.server import BaseHTTPRequestHandler
 from sabor_con_flow.wsgi import application
 from asgiref.wsgi import WsgiToAsgi
 
-app = WsgiToAsgi(application) 
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        
+        # Convert WSGI app to ASGI
+        asgi_app = WsgiToAsgi(application)
+        
+        # Handle the request
+        response = asgi_app(self.environ, self.start_response)
+        self.wfile.write(response)
+
+handler = Handler 
